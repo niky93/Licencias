975e6bf916c2a54895eb3c62d712012232338c7e
Nicole Ricaldi <nikyricaldi@gmail.com>
2024-10-20 17:53:23 -0400

    chore: add home view

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+ 
+@section('title', 'Inicio - Licencias de Funcionamiento')
+ 
+@section('content')
+<div class="bg-light p-5 rounded">
+  <h1>Bienvenido al Portal de Licencias de Funcionamiento</h1>
+  <p class="lead">Sabemos lo importante que es para tu negocio operar dentro del marco legal, por eso, nuestro objetivo es brindarte un servicio eficiente y accesible. Aquí encontrarás toda la información y las herramientas necesarias para que tu empresa cumpla con los requisitos establecidos y continúes creciendo en nuestra vibrante ciudad. ¡Estamos aquí para ayudarte a dar el siguiente paso hacia el éxito!</p>
+  <a class="btn btn-lg btn-primary" href="/docs/5.0/components/navbar/" role="button">Solicitar Licencia &raquo;</a>
+</div>
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,54 @@
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <title>@yield('title', 'Welcome')</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <!-- Favicons -->
+    {{-- <link rel="apple-touch-icon" href="/docs/5.0/assets/img/favicons/apple-touch-icon.png" sizes="180x180">
+    <link rel="icon" href="/docs/5.0/assets/img/favicons/favicon-32x32.png" sizes="32x32" type="image/png">
+    <link rel="icon" href="/docs/5.0/assets/img/favicons/favicon-16x16.png" sizes="16x16" type="image/png">
+    <link rel="manifest" href="/docs/5.0/assets/img/favicons/manifest.json">
+    <link rel="mask-icon" href="/docs/5.0/assets/img/favicons/safari-pinned-tab.svg" color="#7952b3">
+    <link rel="icon" href="/docs/5.0/assets/img/favicons/favicon.ico"> --}}
+    <meta name="theme-color" content="#7952b3">
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+
+      /* Show it's not fixed to the top */
+      body {
+        min-height: 100vh;
+      }
+
+    </style>
+  </head>
+  <body>
+    
+    @include('components.navbar')
+
+    <main class="container">
+      @yield('content')
+    </main>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script> 
+  </body>
+</html>
